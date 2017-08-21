33d9be04b74b8e87efe98cf6ca1ac6b2b4c7e323
e_walker <e_walker2@mail.ru>
2017-08-21 10:09:40 +0000

    table data

---
+++ models/table.js
@@ -0,0 +1,145 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+let RecordFieldSchema = new Schema({
+    fieldName: String,
+    value: String
+});
+let RecordSchema = new Schema({
+
+    tableName: { type: String, required: true },
+    record: [RecordFieldSchema],
+
+
+});
+
+
+RecordSchema.methods.getField= function (fieldName) {
+    for(let i=0;i<this.record.length;i++) {
+        if (this.record[i].fieldName == fieldName) {
+            return this.record[i];
+        }
+    }
+    return null;
+}
+
+RecordSchema.statics.isAccepted = function(fieldSet, filter) {
+    if (fieldSet.length < filter.length) {
+        return false;
+    }
+    for (let i = 0; i < filter.length; i++) {
+        let found = false;
+        for (let j = 0; j < fieldSet.length; j++) {
+            if (fieldSet[j].fieldName == filter[i].fieldName) {
+                if (!fieldSet[j].value.startsWith(filter[i].value)) {
+                    
+                    return false;
+                } else {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            
+            return false;
+        }
+        
+    }
+    return true;
+
+}
+
+
+RecordSchema.statics.getPage = function(tableName, pageNr, pageSize, filter, order) {
+    
+    
+
+    return new Promise(function(resolve, reject) {
+        //have to fetch all data due records value in nested array
+        RecordsModel.find({ tableName: tableName }, function(err, records) {
+
+         
+
+            if (err) {
+                reject(err);
+            }
+            else {
+
+                if (records.length > 0) {
+
+                    let recordData = {
+                        length: 0,
+                        records: []
+                    }
+                    
+                    let recs = [];
+                    if (typeof filter !== 'undefined') {
+                        for (let i = 0; i < records.length; i++) {
+                            if (RecordsModel.isAccepted(records[i].record,filter)) {
+                                recs.push(records[i]);
+                            }
+                        }
+                    }else {
+                        recs = records;
+                    }
+
+
+                    if (typeof order !==undefined) {
+                        recs.sort(function (a,b) {
+                            
+                           let direction = (order.isAsc)?1:-1;    
+                           let firstRecField = a.record.find((elem)=>elem.fieldName==order.fieldName);
+                           
+                           let secondRecField = b.record.find((elem)=>elem.fieldName==order.fieldName);
+                          
+                           if (typeof firstRecField == 'undefined') {
+                               return -1*direction;
+                           }else if (typeof secondRecField == 'undefined') {
+                               return 1*direction;
+                           }else {
+                               return direction*((firstRecField.value<secondRecField.value)?-1:1);
+                           }
+                        });
+                    }
+                    
+                    if (typeof pageNr != 'undefined' && typeof pageSize != 'undefined') {
+                        recordData.pageNr = pageNr;
+                        recordData.pageSize = pageSize;
+                        
+                        let start = +pageNr*pageSize;
+                        let end = (+pageNr+1)*pageSize;
+                        recordData.pagesRemaining = Math.ceil((recs.length-end)/pageSize);
+                        
+                        if (start>=recs.length) {
+                            recs = [];
+                        }else {
+                            recs = recs.slice(start,end);
+                        }
+                        
+
+                    }
+                    recordData.records = recs;
+                    recordData.length = recs.length;
+                    resolve(recordData);
+
+                }
+                else {
+
+                    let error = new Error();
+                    error.status = 404;
+                    reject(error);
+                }
+
+            }
+
+        });
+    });
+
+
+
+
+}
+var RecordsModel = mongoose.model('Records', RecordSchema);
+module.exports = RecordsModel;
---
+++ routes/table.js
@@ -0,0 +1,150 @@
+let express = require('express');
+let router = express.Router();
+
+let table = require('../models/table');
+
+
+/* GET users listing. */
+router.get('/:tablename', function(req, res, next) {
+
+
+    let pageNr = undefined;
+    let pageSize = undefined;
+    let filter = [];
+    let order = undefined;
+
+    if (req.query.page != 'undefined' && req.query.size != 'undefined') {
+        pageNr = req.query.page;
+        pageSize = req.query.size;
+
+    }
+
+    if (req.query.order != 'undefined') {
+        order = { fieldName: req.query.order };
+        order.isAsc = (typeof req.query.desc == 'undefined');
+    }
+    for (var i in req.query) {
+        if (i.startsWith('filter')) {
+            let filterItem = {
+                fieldName: i.substring(6),
+                value: req.query[i]
+            }
+            filter.push(filterItem);
+        }
+
+    }
+    if (filter.length == 0) {
+        filter = undefined;
+    }
+
+    table.getPage(req.params.tablename, pageNr, pageSize, filter, order).then(function(value) {
+            res.json(value);
+        },
+        function(value) {
+            next(value);
+        }
+
+    );
+
+
+
+});
+
+router.post('/', function(req, res, next) {
+
+    if (typeof req.query.table_name !== 'undefined') {
+        let tableName = req.query.table_name;
+        let fieldSet = [];
+        for (let i in req.query) {
+            if (i != 'table_name') {
+                let item = {
+                    fieldName: i,
+                    value: req.query[i]
+                }
+                fieldSet.push(item);
+            }
+        }
+        let rec = new table;
+        rec.tableName = tableName;
+        rec.record = fieldSet;
+        rec.save(function(err, savedRec) {
+            if (err) {
+                let error = new Error();
+                error.status = 500;
+                next(error);
+            }
+            else {
+                res.json({ status:'ok', record_id: savedRec.id });
+            }
+        });
+
+
+    }
+    else {
+        let error = new Error();
+        error.status = 400;
+        next(error);
+    }
+
+});
+ 
+router.patch('/record/:recordId', function(req, res, next) {
+
+
+
+        table.findById(req.params.recordId, function(err, record) {
+           
+        if (err) {
+            let error = new Error();
+            error.status = 500;
+            next(error);
+
+        } else {
+            for (let i in req.query) {
+                let field = record.getField(i);
+                if (field!=null) {
+                   field.value = req.query[i];    
+                } else {
+                    let error = new Error();
+                    error.status = 400;
+                    next(error);
+                    return;
+                }
+                 
+                
+            }
+            record.save(function (err,record) {
+               if(err) {
+                 let error = new Error();
+                    error.status = 500;
+                    next(error);  
+               } else {
+                   res.json({status:'ok',record_id:record.id});
+               } 
+            });
+            
+        }
+    });
+
+
+});
+
+router.delete('/record', function(req, res, next) {
+    if (req.query.record_id!=='undefined') {
+       table.remove({_id:req.query.record_id},function(err,record){
+           if(err) {
+                 let error = new Error();
+                    error.status = 500;
+                    next(error);  
+               } else {
+                   res.json({status:'ok',record_id:req.query.record_id});
+               } 
+       }); 
+    }
+});
+
+
+
+
+
+module.exports = router;
